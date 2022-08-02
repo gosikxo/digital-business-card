50f4f6bbc27e96f5c768a100c47c6ffca36372ea
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-08-02 14:10:10 +0200

    Add components

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='about-container'>
+        <h1 className='name'>Malgorzata Brozek</h1>
+        <h3>Fronted Developer</h3>
+        <p>malgorzatabrozek.website</p>
+    </div>
+  )
+}
---
+++ src/components/Info.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Info = () => {
+  return (
+    <div className='info-container'>
+        <div className='cat-img'></div>
+    </div>
+  )
+}
