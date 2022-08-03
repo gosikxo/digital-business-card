dfdc25a8264ae300ff87f47c4903b7534ad63efa
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-08-03 18:07:45 +0200

    Add icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import {FaTwitter, FaFacebook, FaInstagram, FaGithub} from 'react-icons/fa'
+
 
 export const Footer = () => {
   return (
-    <div className='footer'></div>
+    <div className='footer'>
+      <FaTwitter className='icon'/>
+      <FaFacebook className='icon'/>
+      <FaInstagram className='icon'/>
+      <FaGithub className='icon'/>
+    </div>
   )
 }
